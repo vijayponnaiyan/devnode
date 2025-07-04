9cae19a7417a5fcbc9abea631333582e9b94d22d
vijayponnaiyan <isreallit@gmail.com>
2025-07-04 19:49:07 +0530

    router - 2

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.get(/.*fly$/, (req, res) => res.send("Hello  fools"));
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params); // correctly logs the URL parameters
+  res.send({
+    FirstName: "vijay",
+    age: "30",
+    address: "12/13 MCSR Street, Kumbakonam", // corrected spelling
+  });
+});
+
 app.use("/hello", (req, res) => res.send("Hello  fools"));
 
-app.use("/fool", (req, res) => res.send("cool"));
+app.use("/fool/2", (req, res) => res.send("cool amder codbura"));
 app.use("/test", (req, res) => res.send("Hello World!"));
-app.use((req, res) => res.send("Hello World!    super"));
+
+app.get("/", (req, res) => {
+  res.send("super");
+});
+
+// app.use((req, res) => res.send("Hello World!    super"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
